b7cd83152cfda37e6d429ef0372c5b537da4a197
Rafał Nowak <rafal.nowak@tooploox.com>
2020-07-02 12:46:26 +0200

    added version of OpenVino

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
-# set your own url to OpenVino toolkit package like
-# http://registrationcenter-download.intel.com/______/irc_nas/_______/l_openvino_toolkit_p________.tgz
-#
-# see https://docs.openvinotoolkit.org/latest/_docs_install_guides_installing_openvino_docker_linux.html
-#
-PACKAGE_URL=http://registrationcenter-download.intel.com/akdlm/irc_nas/16612/l_openvino_toolkit_p_0000.0.000.tgz
+# Please replace direct link to the Intel® Distribution of OpenVINO™ toolkit
+# package to the latest version in the package_url argument. You can copy the
+# link from the Intel® Distribution of OpenVINO™ toolkit download page after 
+# registration.
+PACKAGE_URL=http://registrationcenter-download.intel.com/akdlm/irc_nas/16670/l_openvino_toolkit_p_2020.3.194.tgz
+
