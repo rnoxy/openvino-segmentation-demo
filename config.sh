3d6e28f777ccea56f1ae9655a36861a8086b5481
Rafał Nowak <rafal.nowak@tooploox.com>
2020-07-02 12:41:24 +0200

    added package_url config parameter

---
+++ config.sh
@@ -0,0 +1,6 @@
+# set your own url to OpenVino toolkit package like
+# http://registrationcenter-download.intel.com/______/irc_nas/_______/l_openvino_toolkit_p________.tgz
+#
+# see https://docs.openvinotoolkit.org/latest/_docs_install_guides_installing_openvino_docker_linux.html
+#
+PACKAGE_URL=http://registrationcenter-download.intel.com/akdlm/irc_nas/16612/l_openvino_toolkit_p_0000.0.000.tgz
